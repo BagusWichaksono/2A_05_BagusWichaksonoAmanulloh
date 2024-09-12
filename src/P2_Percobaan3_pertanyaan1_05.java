9b21ea53e83322d78bb117165f86abf6ce59a2d2
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2024-09-12 20:14:00 +0700

    Data Mahasiswa

---
+++ src/P2_Percobaan3_pertanyaan1_05.java
@@ -0,0 +1,15 @@
+public class P2_Percobaan3_pertanyaan1_05 {
+    public static void main (String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Bagus Wichaksono Amanulloh");
+        System.out.println("NIM\t\t: 244107020238");
+        System.out.println("Kelas\t\t: 1G");
+        System.out.println("No Absen\t: 05");
+        System.out.println("Prodi\t\t: Teknik Informatika");
+        System.out.println("Alamat\t\t: Singosari");
+        System.out.println("No HP\t\t: 081336521884");
+        System.out.println("Hobi\t\t: Pengamat Otomotif");
+    }
+}
