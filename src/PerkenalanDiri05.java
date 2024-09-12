c6c92c41c56b45ec515fb74ce9f3b38506d5f523
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2024-09-12 19:58:35 +0700

    Data KRS

---
+++ src/PerkenalanDiri05.java
@@ -0,0 +1,23 @@
+public class PerkenalanDiri05 {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t\t\tKARTU RENCANA STUDI (KRS)\n");
+        System.out.println("NIM\t\t\t\t: 244107020238");
+        System.out.println("NAMA\t\t\t\t: BAGUS WICHAKSONO AMANULLOH");
+        System.out.println("PROGRAM STUDI\t\t\t: Diploma IV Teknik Informatika");
+        System.out.println("KELAS\t\t\t\t: 1G\n");
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+        System.out.println("|\tKODE MK\t\t|\tMATA KULIAH\t\t\t\t|\tSKS\t|\tJAM\t|");
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+        System.out.println("|\tRT1241001\t| Pancasila\t\t\t\t\t|\t2\t|\t2\t|");
+        System.out.println("|\tRT1241002\t| Konsep Teknologi Informasi\t\t\t|\t2\t|\t4\t|");
+        System.out.println("|\tRT1241003\t| Critical Thinking dan Problem Solving\t\t|\t2\t|\t4\t|");
+        System.out.println("|\tRT1241004\t| Matematika Dasar\t\t\t\t|\t3\t|\t6\t|");
+        System.out.println("|\tRT1241005\t| Rekayasa Perangkat Lunak\t\t\t|\t2\t|\t4\t|");
+        System.out.println("|\tRT1241006\t| Dasar Pemrogaman\t\t\t\t|\t2\t|\t4\t|");
+        System.out.println("|\tRT1241007\t| Praktikum Dasar Pemrogaman\t\t\t|\t3\t|\t6\t|");
+        System.out.println("|\tRT1241008\t| Keselamatan dan Kesehatan Kerja\t\t|\t2\t|\t4\t|");
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t|\t\t\tJUMLAH\t\t\t|\t18\t|\t34\t|");
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+    }
+}
