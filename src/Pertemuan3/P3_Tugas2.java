e958a5ea61dacd0852a13fb4fbee2291b010a5a1
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2024-10-02 19:08:24 +0700

    P5_Percobaan1

---
+++ src/Pertemuan3/P3_Tugas2.java
@@ -0,0 +1,29 @@
+package Pertemuan3;
+
+import java.util.Scanner;
+
+/**
+ * P3_Tugas2
+ */
+public class P3_Tugas2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int jumlahJamKerja, upahPerJam;
+        double gajiKaryawan, bonusGaji, gajiSebelumPajak, gajiSetelahPajak, gajiBersih;
+        System.out.print("Masukkan jumlah jam kerja karyawan: ");
+        jumlahJamKerja = input.nextInt();
+        System.out.print("Masukkan upah per jam: ");
+        upahPerJam = input.nextInt();
+        gajiKaryawan = jumlahJamKerja * upahPerJam;
+        System.out.println("Gaji Karyawan: " + gajiKaryawan);
+        bonusGaji = gajiKaryawan * 0.1;
+        System.out.println("Bonus Gaji: " + bonusGaji);
+        gajiSebelumPajak = gajiKaryawan + bonusGaji;
+        System.out.println("Gaji Sebelum Pajak: " + gajiSebelumPajak);
+        gajiSetelahPajak = bonusGaji * 0.05;
+        System.out.println("Gaji Setelah Pajak: " + gajiSetelahPajak);
+        gajiBersih = (int) (gajiKaryawan - gajiSetelahPajak);
+        System.out.println("Gaji Bersih sebesar: " + gajiBersih);
+    }
+}
---
+++ src/Pertemuan4/Quiz05.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Quiz05 {
+    public static void main(String[] args) {
+        System.out.println("Nama\t: Bagus Wichaksono Amanulloh");
+        System.out.println("NIM\t: 244107020238");
+        System.out.println("Absen\t: 05");
+        System.out.println("=============== QUIZ 1 TI - 1G ===========");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan tanggal berangkat: ");
+        int inputTanggalBerangkat = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Masukkan bulan berangkat: ");
+        int inputBulanBerangkat = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Masukkan tahun berangkat: ");
+        int inputTahunBerangkat = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Tanggal berangkat " + inputTanggalBerangkat + "-" + inputBulanBerangkat + "-" + inputTahunBerangkat);
+        System.out.println();
+        System.out.print("Masukkan tanggal pulang: ");
+        int inputTanggalPulang = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Masukkan bulan pulang: ");
+        int inputBulanPulang = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Masukkan tahun pulang: ");
+        int inputTahunPulang = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Tanggal kepulangan " + inputTanggalPulang + "-" + inputBulanPulang + "-" + inputTahunPulang);
+        int totalLama = (30 - inputTanggalBerangkat + inputTanggalPulang) + ((12 - inputBulanBerangkat + inputBulanPulang) * 30) + ((inputTahunPulang - inputTahunBerangkat) * 360);
+
+        int tanggal = ((totalLama % 360) % 30);
+        int bulan = ((totalLama % 360) / 30) - 1;
+        int tahun = totalLama / 360 - 1;
+
+        System.out.println("Lama Student Exchange Anda adalah : " + tahun + " tahun " + bulan + " bulan " + tanggal + " hari ");
+
+       
+
+
+    }
+    
+}
