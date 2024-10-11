3ae4ab268d884b40235f5f572b2468b613492340
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2024-10-11 16:39:18 +0700

    P6_Percobaan1

--- src/Pertemuan6/Pemilihan2Percobaan105.java
+++ src/Pertemuan6/Pemilihan2Percobaan105.java
@@ -10,17 +10,10 @@ public class Pemilihan2Percobaan105 {
         int tahun = input05.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) == 0) {
-                if ((tahun % 400) == 0) {
-                    System.out.println("Tahun Kabisat");
-                } else {
-                    System.out.println("Bukan Tahun Kabisat");
-                }
-            } else {
+            if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-            }
-        } else {
+        } else
             System.out.println("Bukan Tahun Kabisat");
+            }
         }
     }
-}
