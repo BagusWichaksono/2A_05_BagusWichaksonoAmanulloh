24423ba595415a8eb8b9eefb034720efe98f39fc
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2024-11-29 14:04:19 +0700

    P13_Percobaan2_PertanyaanNo5

--- src/Pertemuan13/Kafe05.java
+++ src/Pertemuan13/Kafe05.java
@@ -1,13 +1,21 @@
 package Pertemuan13;
 
 public class Kafe05 {
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
 
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode Promo berlaku: Diskon 50% untuk setiap pembelian!");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode Promo berlaku: Diskon 30% untuk setiap pembelian!");
+        } else {
+            System.out.println("Kode promo tidak valid.");
+        }
+        
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Capuccino - Rp 20,000");
@@ -16,9 +24,12 @@ public class Kafe05 {
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
+
+
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
+
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
     }
 }
