443bf6af7965235f749900251e04accc22d24faf
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-03-20 11:56:51 +0700

    Mahasiswa05

--- src/Praktikum05/Sorting05.java
+++ src/Praktikum05/Sorting05.java
@@ -32,4 +32,30 @@ public class Sorting05 {
         }
         System.out.println();
     }
+
+    void SelectionSort() {
+        for (int i = 0; i < jumData - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < jumData; j++) {
+                if (data[j] < data[min]) {
+                    min = j;
+                }
+            }
+            int temp = data[i];
+            data[i] = data[min];
+            data[min] = temp;
+        }
+    }
+
+    void InsertionSort() {
+        for (int i = 1; i <= data.length - 1; i++) {
+            int temp = data[i];
+            int j = i - 1;
+            while (j >= 0 && data[j] > temp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = temp;
+        }
+    }
 }
