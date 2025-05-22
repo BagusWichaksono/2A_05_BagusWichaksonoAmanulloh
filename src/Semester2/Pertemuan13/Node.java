27e1d04bce7a51c583f33b565284f7a6ddab9ba0
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-05-22 09:10:01 +0700

    Percobaan1

---
+++ src/Semester2/Pertemuan13/Node.java
@@ -0,0 +1,12 @@
+package Semester2.Pertemuan13;
+
+public class Node {
+    int data;
+    Node prev, next;
+
+    Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
