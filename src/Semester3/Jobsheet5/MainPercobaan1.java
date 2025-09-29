fe05f4c74ae88523c172b0cfffeed617927ef5f7
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-09-29 12:24:22 +0700

    P2

--- src/Semester3/Jobsheet5/MainPercobaan1.java
+++ src/Semester3/Jobsheet5/MainPercobaan1.java
@@ -9,7 +9,6 @@ public class MainPercobaan1 {
         Processor p1 = new Processor();
         p1.setMerk("Intel i5");
         p1.setCache(4);
-        
         Laptop L1 = new Laptop();
         L1.setMerk("Thinkpad");
         L1.setProc(p1);
