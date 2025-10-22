f8488ac55f3c246213730ede926927dd44934ceb
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-10-22 08:46:01 +0700

    p1

---
+++ src/Semester3/Jobsheet8/Kucing.java
@@ -0,0 +1,9 @@
+package Semester3.Jobsheet8;
+
+public class Kucing extends Hewan {
+    
+    @Override
+    public void bergerak() {
+        System.out.println("Berjalan dengan KAKI, \"tap..tap..\"");
+    }
+}
